6167e19e15ee34327a9df43e64dd8d0fc414e112
Sakshi Waghmare <your-email@example.com>
2025-02-14 15:10:15 +0530

    update

---
+++ Inheritance 2/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    
+    public static void main(String args[]){
+
+        Vehicle V = new Vehicle();
+        Car c = new Car();
+        c.drive();
+        V.drive();
+   
+    }
+}
