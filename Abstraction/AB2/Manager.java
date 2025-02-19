e1ed6b60dfa9a6ec378b4974d4f59898a1c4ddb2
Sakshi Waghmare <your-email@example.com>
2025-02-19 10:20:51 +0530

    update

--- Abstraction/AB2/Manager.java
+++ Abstraction/AB2/Manager.java
@@ -1,7 +1,6 @@
 class Manager extends Employee {
     public int bonus;
 
-    // Ensure actualSalary is an int
     public Manager(String name, int id, int actualSalary, int bonus) {
         super(name, id, actualSalary); 
         this.bonus = bonus;
@@ -9,14 +8,14 @@ class Manager extends Employee {
 
     @Override
     public int calculateSalary() {
-        return actualSalary + bonus; // Now both are int, no conversion needed
+        return actualSalary + bonus; 
     }
 
     @Override
     public void displayInfo() {
-        System.out.println("Manager Info:");
         System.out.println("ID: " + id);
         System.out.println("Name: " + name);
         System.out.println("Salary: " + calculateSalary());
     }
 }
+
