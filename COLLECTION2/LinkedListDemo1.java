ed2d8f78d38aee9a9f30a12811082762ae699eaf
Sakshi Waghmare <your-email@example.com>
2025-02-24 17:36:33 +0530

    linkedlist1

---
+++ COLLECTION2/LinkedListDemo1.java
@@ -0,0 +1,21 @@
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class LinkedListDemo1{
+
+    public static void main(String[] args) {
+        
+       
+        LinkedList<Object> LL = new LinkedList<>();
+        LL.add(10);
+        LL.add(null);
+        LL.add(10);
+        LL.add("String");
+
+        Iterator itr = LL.iterator();
+        while(itr.hasNext()){
+            System.out.println(itr.next());
+        }
+    }
+}
